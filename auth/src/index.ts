529c45ded60ec78dd59f8c9fdd8b6cd4f78fb09c
Harshit Kanodia <harshit.kanodia@checkapp.in>
2021-06-01 04:31:35 +0530

    Added mongodb deployment Added mongoose and mongoose types

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/singin';
-import {errorHandler} from './middlewares/error.handler';
+import { errorHandler } from './middlewares/error.handler';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -15,7 +16,21 @@ app.use(signoutRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(errorHandler);
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log("Connected to mongodb!");
+    } catch (err) {
+        console.log(err);
+    }
 
-app.listen(3000,()=>{
+}
+
+start();
+app.listen(3000, () => {
     console.log("Server running on port 3000!!");
 })
