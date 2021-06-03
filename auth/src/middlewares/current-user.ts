c156b4bb7b68156341866d0a40541768f63f84de
Harshit Kanodia <harshit.kanodia@checkapp.in>
2021-06-03 11:40:38 +0530

    Added common handling for current user

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,29 @@
+import {Request,Response,NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+declare global{
+    namespace Express{
+        interface Request{
+            currentUser? : UserPayload
+        }
+    }
+}
+export const getCurrentUser = (req:Request,res:Response,next:NextFunction)=>{
+    if(req.session?.jwt){
+        try{
+            const payload = (jwt.verify(req.session.jwt,process.env.JWT_TOKEN!) as UserPayload);
+            req.currentUser = payload;
+        }catch(err){
+            res.send({
+                currentUser: null
+            });
+        }
+    } else{
+        next();
+    }
+    next();
+}
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import BadRequestError from '../errors/BadRequestError';
+import { getCurrentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser',(req,res)=>{
-    res.send("Hi there!");
+router.get('/api/users/currentuser',getCurrentUser,(req,res)=>{
+    if(req.currentUser){
+        res.status(200).send({currentUser : req.currentUser});
+    } else {
+        res.send({currentUser:null});
+    }
 });
 
 export { router as currentUserRouter };
