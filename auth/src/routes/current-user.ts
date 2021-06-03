66591b5dc5e212f420ebc1f2f95ff93194f33a6d
Harshit Kanodia <harshit.kanodia@checkapp.in>
2021-06-03 11:47:37 +0530

    Added middleware for user auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,15 +2,12 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import BadRequestError from '../errors/BadRequestError';
 import { getCurrentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser',getCurrentUser,(req,res)=>{
-    if(req.currentUser){
-        res.status(200).send({currentUser : req.currentUser});
-    } else {
-        res.send({currentUser:null});
-    }
+router.get('/api/users/currentuser',getCurrentUser,requireAuth,(req,res)=>{
+    res.status(200).send({currentUser : req.currentUser});
 });
 
 export { router as currentUserRouter };
